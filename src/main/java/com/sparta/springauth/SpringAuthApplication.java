caba29c79ae38db64a526bc807f7b8be5df7c628
codingjang <jjangga813@naver.com>
2024-11-17 23:45:04 +0900

    init connit

---
+++ src/main/java/com/sparta/springauth/SpringAuthApplication.java
@@ -0,0 +1,17 @@
+package com.sparta.springauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 인증 기능 임시 제외
+public class SpringAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthApplication.class, args);
+
+
+	}
+
+}
