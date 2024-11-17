1c8716dce67656ebf8e564529d7e3e6ecb8e2f45
codingjang <jjangga813@naver.com>
2024-11-17 23:45:39 +0900

    PasswordConfig 정의 PasswordEncoder 빈 등록

---
+++ src/main/java/com/sparta/springauth/config/PasswordConfig.java
@@ -0,0 +1,14 @@
+package com.sparta.springauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {  // passwordEncoder 라는 이름의 Bean 으로 등록됨
+        return new BCryptPasswordEncoder();
+    }
+}
